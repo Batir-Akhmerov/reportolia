9772eb6f837e274ad71012ce7b19b5a725ba87cf
Akhmerov <Akhmerov@RepDevPC.CitadelDomain>
2016-09-02 15:23:13 -0400

    Fixed Static Filters in Nested Select Columns

---
+++ reportolia/src/test/java/com/reportolia/core/Report50_StaticFilterInNestedSelect_Test.java
@@ -0,0 +1,38 @@
+package com.reportolia.core;
+
+import org.junit.Test;
+
+/**
+ * 
+ * The Report4_StaticFilterOperands_Test class
+ *
+ * @author Batir Akhmerov
+ * Created on Dec 11, 2015
+ */
+public class Report50_ParameterizedColumn_Test extends BaseReportTest{
+
+	@Test
+    public void reportTest() {
+			
+		testReportSql("Simple Table Calculated Column Test",
+			40L, 
+			" SELECT  "
+					+ "tbl50.name "
+					+ ",tbl50.price "
+					+ ", ( SELECT TOP 1 "
+							+ "_tbl50_2.quantity "
+						+ "FROM orders _tbl50_2 "
+						+ "WHERE _tbl50_2.product_id = tbl50.id "
+					+ ") "
+					+ ", ( SELECT TOP 1 _tbl50_2.quantity FROM orders _tbl50_2 ) "
+			+ "FROM products tbl50 "
+			+ "WHERE tbl50.name = ? "
+			+ "ORDER BY  tbl50.name DESC "
+		, "Test Product");
+		
+    }
+	
+	
+	
+	
+}
